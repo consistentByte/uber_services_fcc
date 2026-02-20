d6850ad71f82f2f1fcfb759215d6d1e3d7117e6e
Saurabh Pandey <consistentByte@gmail.com>
2026-02-20 11:31:35 +0530

    4- mapping host port with mongo container's port, adding MongooseModule and connection string to Logging-serviceModule

--- apps/logging-service/src/logging-service.module.ts
+++ apps/logging-service/src/logging-service.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LoggingServiceController } from './logging-service.controller';
 import { LoggingServiceService } from './logging-service.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  // initializing a dynamic module for Mongoose.
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/logs_db')],
   controllers: [LoggingServiceController],
   providers: [LoggingServiceService],
 })
